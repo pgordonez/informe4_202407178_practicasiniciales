cea13d7f97fc28cb9c181ed6d14fb390334096c8
Pablo Ordoñez <3044029070114@ingenieria.usac.edu.gt>
2026-03-19 10:47:42 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,51 @@
 const express = require('express');
-const app = express();
+const mysql = require('mysql2');
 
-app.get('/', (req, res) => {
-  res.send('FUNCIONA');
+const app = express();
+app.use(express.json());
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'informe4_practica'
+});
+
+db.connect(err => {
+  if (err) throw err;
+  console.log('Conectado a MySQL');
+});
+
+app.get('/usuarios', (req, res) => {
+  db.query('SELECT * FROM usuarios', (err, result) => {
+    if (err) return res.send(err);
+    res.json(result);
+  });
+});
+
+app.post('/publicaciones', (req, res) => {
+  const { usuario_id, tipo, referencia_id, mensaje } = req.body;
+
+  const sql = 'INSERT INTO publicaciones (usuario_id, tipo, referencia_id, mensaje) VALUES (?, ?, ?, ?)';
+  
+  db.query(sql, [usuario_id, tipo, referencia_id, mensaje], (err, result) => {
+    if (err) return res.send(err);
+    res.send('Publicación creada');
+  });
 });
 
 app.listen(3000, () => {
-  console.log('Servidor activo en 3000');
+  console.log('Servidor en http://localhost:3000');
+});
+
+app.get('/publicaciones', (req, res) => {
+  const sql = 'SELECT * FROM publicaciones';
+
+  db.query(sql, (err, result) => {
+    if (err) {
+      console.log(err);
+      return res.send('Error al obtener publicaciones');
+    }
+    res.json(result);
+  });
 });
